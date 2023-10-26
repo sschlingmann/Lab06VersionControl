68454d337379b4ac9b40be2089b3d2a44af2715b
joshuaberelson <joshberelson@gmail.com>
2023-10-25 20:46:26 -0400

    added decode function

--- version_control.py
+++ version_control.py
@@ -7,22 +7,27 @@ def encode(orig_num):
         encoded_num += digit
     return encoded_num
 
-def decode(encoded_num):
+def decode(encoded_num):  # Takes in integer password as a string and decodes it back to original string. -Scott
     result = ''
     for i in encoded_num:
-        result += str((int(i)-3))
+        result += str((int(i)+7) % 10)
     return result
 
-# Now we will define the main function. (Code up to line 19 programmed by Scott Schlingmann)
+
+# Now we will define the main function.
 def main():
     option = None
     while option != 3:
         print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
         option = int(input('Please enter an option: '))
-        orig_num = input('Please enter your password to encode: ')
-        encoded_num = encode(orig_num)
-        print('Your password has been encoded and stored!\n')
+        if option == 1:
+            orig_num = input('Please enter your password to encode: ')
+            encoded_num = encode(orig_num)
+            print('Your password has been encoded and stored!\n')
 
+
+        if option == 2:
+            print(f"The encoded password is {encoded_num}, and the original password is {decode(encoded_num)}.")
 
 
 if __name__ == '__main__':
